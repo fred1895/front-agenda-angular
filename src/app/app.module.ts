de0fbbc70a6d0695f963e05b197eda98ebd50c74
Fred Almeida <fredericonascimento97@gmail.com>
2020-10-06 17:38:31 -0300

    Estilizando pagina de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 import { ContatoComponent } from './contato/contato.component';
 import { ContatoService } from './contato.service';
 
@@ -15,6 +17,8 @@ import { ContatoService } from './contato.service';
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [
